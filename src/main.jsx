2fbd6f7963b2ea9b54fcedd966d7f8e21bd4e89c
dk10119 <tranduclong092@gmail.com>
2024-04-04 16:41:55 +0300

    add task 5

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App1 from './task1/App.jsx'
 import App2 from './task2/App.jsx'
 import App3 from './task3/App.jsx'
 import App4 from './task4/App.jsx'
+import App5 from './task5/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App1 />
     <App2 />
     <App3 />
-    <App4/>
+    <App4 />
+    <App5 />
   </React.StrictMode>,
 )
