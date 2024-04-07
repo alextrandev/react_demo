7285c5d950ed8af1f74c103e632668bf4c541b81
dk10119 <tranduclong092@gmail.com>
2024-04-07 10:34:16 +0300

    add task 8

---
+++ src/task8/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./styles.css";
+import Form from "./Form";
+import View from "./View";
+
+const App8 = () => {
+
+  return (
+    <main>
+      <h1>Forms and User Input</h1>
+      <Form />
+      <View />
+    </main>
+  );
+};
+
+export default App8;
