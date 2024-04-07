04fd90f5c13d37b95597a3711228045fd3585711
dk10119 <tranduclong092@gmail.com>
2024-04-07 11:11:37 +0300

    pass user inputs data from parent App.jsx to child View.jsx

--- src/task8/App.jsx
+++ src/task8/App.jsx
@@ -4,15 +4,17 @@ import Form from "./Form";
 import View from "./View";
 
 const App8 = () => {
+    const [userInputs, setUserInputs] = useState()
+
     const handleSubmit = (userInputs) => {
-        console.log(userInputs);
+        setUserInputs(userInputs);
     }
 
   return (
     <main>
       <h1>Forms and User Input</h1>
       <Form handleSubmit={handleSubmit}/>
-      <View />
+      <View userDetails={userInputs}/>
     </main>
   );
 };
--- src/task8/View.jsx
+++ src/task8/View.jsx
@@ -1,13 +1,14 @@
-const View = ({ details }) => {
-  if (details) {
-    const { firstname, lastname, role, phone, email } = details;
+const View = ({ userDetails }) => {
+  if (userDetails) {
+    const { firstname, lastname, role, phone, email } = userDetails;
     return (
       <div>
         <h2>Your form inputs</h2>
         <div>
-          <p>
-            {firstname} {lastname} {phone} {email} {role}
-          </p>
+            <p>Name: {firstname} {lastname}</p>
+            <p>Phone number: {phone}</p>
+            <p>E-mail: {email}</p>
+            <p>Role: {role}</p>
         </div>
       </div>
     );
