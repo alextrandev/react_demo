09d4770e8351524f05de5d889d872b1009e144ee
dk10119 <tranduclong092@gmail.com>
2024-04-07 11:19:54 +0300

    add task 9

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import App5 from './task5/App.jsx'
 import App6 from './task6/App.jsx'
 import App7 from './task7/App.jsx'
 import App8 from './task8/App.jsx'
+import App9 from './task9/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -19,5 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App6 />
     <App7 />
     <App8 />
+    <App9 />
   </React.StrictMode>,
 )
